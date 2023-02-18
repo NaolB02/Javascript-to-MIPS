0800a750262da3a565371fd54f9906354527f2b6
NaolB02 <106165221+NaolB02@users.noreply.github.com>
2023-02-18 11:00:50 +0300

    Arithmetic

--- ArithmeticOperations.py
+++ ArithmeticOperations.py
@@ -29,26 +29,3 @@ def arithmetic_converter(ast, num = 0):
         num -= 1
         return stmt
 
-
-
-js = '''
-a > 5
-'''
-ast= parse(js)
-print(ast)
-# Generate MIPS assembly code
-# code = arithmetic_converter(ast)
-
-# # Print MIPS assembly code
-# print(code)
-
-
-# ast = {'type': 'BinaryExpression', 'operator': '+', 'left': {'type': 'BinaryExpression', 'operator': '+', 'left': {'type': 'Literal', 'value': 9.0, 'raw': '9'}, 'right': {'type': 'Literal', 'value': 4.0, 'raw': '4'}}, 'right': {'type': 'Literal', 'value': 6.0, 'raw': '6'}}
-# code = arithmetic_converter(ast)
-# print(code)
-
-# ast = {'type': 'BinaryExpression', 'operator': '+', 'left': {'type': 'BinaryExpression', 'operator': '-', 'left': {'type': 'Literal', 'value': 9.0, 'raw': '9'}, 'right': {'type': 'Literal', 'value': 3.0, 'raw': '3'}}, 'right': {'type': 'BinaryExpression', 'operator': '/', 'left': {'type': 'Literal', 'value': 4.0, 'raw': '4'}, 'right': {'type': 'Literal', 'value': 6.0, 'raw': '6'}}}
-# ast = {'type': 'BinaryExpression', 'operator': '+', 'left': {'type': 'Literal', 'value': 9.0, 'raw': '9'}, 'right': {'type': 'BinaryExpression', 'operator': '/', 'left': {'type': 'Literal', 'value': 4.0, 'raw': '4'}, 'right': {'type': 'Literal', 'value': 6.0, 'raw': '6'}}}
-ast = {'type': 'BinaryExpression', 'operator': '+', 'left': {'type': 'BinaryExpression', 'operator': '-', 'left': {'type': 'BinaryExpression', 'operator': '+', 'left': {'type': 'Literal', 'value': 9.0, 'raw': '9'}, 'right': {'type': 'Literal', 'value': 2.0, 'raw': '2'}}, 'right': {'type': 'Literal', 'value': 3.0, 'raw': '3'}}, 'right': {'type': 'BinaryExpression', 'operator': '*', 'left': {'type': 'Literal', 'value': 4.0, 'raw': '4'}, 'right': {'type': 'Literal', 'value': 6.0, 'raw': '6'}}}
-code = arithmetic_converter(ast)
-print(code)
